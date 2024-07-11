c0a114d16624a7bb4e72a1838c1e23f2219762fb
servedoktay <49237269+servedoktay@users.noreply.github.com>
2024-07-11 05:51:04 +0300

    Add files via upload

---
+++ 3.13_Array Class Metotları/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._13_Array_Class_Metotları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array dizi|tanimlama
+            //int[] dizi = new int[5];
+            int[] sayilar = new int[] { 5, 3, 8, 10, 2,18,23,44,55,6,34,19 }; //alt2
+
+            var numbers = Array.CreateInstance(typeof(int), sayilar.Length); //alt3
+            //atamasi
+            //numbers.SetValue(-5, 0); //eleman ve index 5 eleman 0 index
+            //numbers.SetValue(3, 1);
+            //numbers.SetValue(8, 2);
+            //numbers.SetValue(10, 3);
+            //numbers.SetValue(-2, 4);
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    numbers.SetValue(sayilar[i], i);//numbersa sayilardaki elemanlari atmak
+            //}
+            //ÇEVRİMSİZ KOPYALAMA
+            sayilar.CopyTo(numbers,0); //(nereye kopyalanmali ve nerden baslamali)
+
+            var arr = new ArrayList(sayilar); //tanim4
+
+            arr.Sort(); //tanim 4 de sadece direkt sort ediyor 
+            //PEKİ DİZİDE NASİL YAPILIR
+            Array.Sort(sayilar); //sayilar siraladi
+            Array.Sort(numbers);
+
+            Array.Clear(sayilar,2,3);//eleman silme //(silinecek baslancak index kac eleman) 
+
+            //belirli eleman listenin kacinci elamanidir
+            var x=Array.IndexOf(sayilar, 44); //dizi ne arıyorum
+            Console.WriteLine(x);
+            Console.WriteLine(Array.IndexOf(sayilar, 100)); //olmayan bisey ararsa -1 basar
+            
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                //Console.WriteLine("sayilar[{0}]={1}-numbers[{0}]={2}",
+                //    i,
+                //    sayilar[i],
+                //    numbers.GetValue(i));
+                Console.WriteLine($"sayilar[{i}]=" +
+                    $"{sayilar[i],3} - " +
+                    $"numbers[{i}]=" +
+                    $"{numbers.GetValue(i),3} " +
+                     $"arr[{i}]={arr[i],3}");
+                //2.gibi dizi olusturursa get demeliyim
+
+
+            }
+            //sirali gelmesini istiyorsam
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ btkZafertekrar1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btkZafertekrar1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //degiskentanimiveatama();
+            int sayi = 5;
+            Console.WriteLine(sayi);
+            Console.WriteLine(sayi + 2);
+            Console.WriteLine(sayi*sayi);
+            Console.WriteLine(sayi-5);
+            Console.ReadKey();
+        }
+
+        private static void degiskentanimiveatama()
+        {
+            //
+            /* basit degisken atama
+            int sayi1;
+            float pi = 3.14f;
+            double pi2 = 3.14;
+            char secim = 'e';
+            string isim = "oktay";
+            bool dogruMu = false;
+            */
+            string isim = "oktay";
+            Console.WriteLine("Merhaba  " + isim);
+            Console.WriteLine("Merhaba Sayin " + isim);
+        }
+    }
+}
