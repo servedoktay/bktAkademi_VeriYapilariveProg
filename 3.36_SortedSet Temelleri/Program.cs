01f05567c06a3c5f102701fb11c405c1af367836
servedoktay <49237269+servedoktay@users.noreply.github.com>
2024-07-16 21:23:26 +0300

    Add files via upload

---
+++ 3.36_SortedSet Temelleri/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._36_SortedSet_Temelleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sorted set
+
+            //tanimlama
+            var list = new SortedSet<string>();
+
+            //ekleme
+            list.Add("Ahmet"); //ya true yada false olucak
+            if (list.Add("Mehmet"))
+            {
+                Console.WriteLine("mehmet eklendi");
+            }
+            Console.WriteLine(list.Add("Ahmet")
+                == true ? "Ahmet Eklendi":"Ekleme Başarisiz");
+            list.Add("Sule");
+            list.Add("Neslihan");
+            list.Add("Fahrettin");
+            list.Add("Fatih");
+            list.Add("\n İsimler Listesi");
+            //dolaşma
+            foreach (string kisi in list) { Console.WriteLine(kisi); }
+
+            //cıkartma
+            list.Remove("Sule");
+            Console.WriteLine("\nSilme İslemi Gerceklesti");
+            foreach (string kisi in list) { Console.WriteLine(kisi); }
+
+            //kucuk a olanlari sil
+            //list.RemoveWhere(deger => deger.Contains("a"));
+            //F İLE baslayanları sil
+            list.RemoveWhere(deger => deger.StartsWith("F"));
+
+            foreach (string kisi in list) { Console.WriteLine(kisi); }
+
+            Console.WriteLine("Eleman Sayisi ...:{0}",list.Count);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 3.37_SortedSet Uygulama/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._37_SortedSet_Uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //random sayılardan olan kume ve o kumenın alt kumesı olusturulsun ve o olusturlan alt ümdeki uyulerın bensersız olsun
+            var sayilar = new List<int>();
+            var r = new Random();
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+            {
+                sayilar.Add(r.Next(5,25)); //aralıgı kac olsun
+                Console.Write($"{sayilar[i],-5}");
+            }
+            Console.WriteLine( );
+
+            var bensersizSayiListesi=new SortedSet<int>(sayilar);
+
+            Console.WriteLine();
+            Console.WriteLine("\nBenzersiz Sayi Listesi\n");
+            foreach (int item in bensersizSayiListesi)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\n");
+            Console.Write("Benzersiz Sayilar  {0}",bensersizSayiListesi.Count);
+            Console.ReadKey();
+        }
+    }
+}
+ 
