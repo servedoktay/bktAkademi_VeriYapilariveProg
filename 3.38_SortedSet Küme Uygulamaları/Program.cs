db00b3473f48ed3790e1397d6d729d4b17f10cae
servedoktay <49237269+servedoktay@users.noreply.github.com>
2024-07-17 19:14:47 +0300

    Add files via upload

---
+++ 3.38_SortedSet Küme Uygulamaları/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._38_SortedSet_Küme_Uygulamaları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sorted kume uygulamaları
+            //var a = new SortedSet<int>() { 1, 2, 3, 4 };
+            var a = new SortedSet<int>(RasgeleSayiUret(10));
+            //var b = new SortedSet<int> { 1, 2, 5, 6 };
+            var b = new SortedSet<int>(RasgeleSayiUret(9));
+
+            Console.WriteLine();
+            Console.WriteLine("A KÜMESi");
+            foreach (var item in a)
+            {
+                Console.Write($"{item}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("B Kümesi");
+            foreach(var item in b)
+            {
+                Console.Write($"{item}  ");
+
+            }
+            Console.WriteLine();
+
+            ////union
+            //a.UnionWith(b);
+            //Console.WriteLine("A Ve B nin Birleşimi");
+            //foreach (var s in a)
+            //{
+            //    Console.Write($"{s}  ");
+
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Eleman Sayisi= "+a.Count);
+            //Console.WriteLine();
+
+            //kesişim
+            //Console.WriteLine("KESİŞİM");
+            //a.IntersectWith(b);
+            //foreach (var s in a)
+            //{
+            //    Console.Write($"{s}  ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Eleman Sayisi= " + a.Count);
+
+            //SADECE A
+            //a.ExceptWith(b); //sadece a
+            //Console.WriteLine();
+            //Console.WriteLine("Sadece A");
+            //foreach (var s in a)
+            //{
+            //    Console.Write($"{s}  ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Toplam Eleman Sayisi= " + a.Count);
+
+            //simetrik
+            a.SymmetricExceptWith(b);
+            Console.WriteLine();
+            Console.WriteLine("Sadece A");
+            foreach (var s in a)
+            {
+                Console.Write($"{s}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Toplam Eleman Sayisi= " + a.Count);
+
+            //A.IsSubsetof(b) aa b nın bır alt kumesimi bool doner altkume kontrolu
+            Console.ReadKey();
+        }
+        static List<int> RasgeleSayiUret(int n)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(r.Next(0, 1000));
+            }
+            return list;
+        }
+    }
+}
---
+++ 3.40_HashSet Uygulama/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._40_HashSet_Uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //hash set
+            var sesliharfler = new HashSet<char>()
+            {
+                'e','ı','ö','o','i','u','ü','b'
+            };
+            koleksiyonyazdir(sesliharfler);
+            //ekeleme
+            sesliharfler.Add('a');
+            koleksiyonyazdir(sesliharfler);
+
+            //cıkartma
+            sesliharfler.Remove('b');
+            koleksiyonyazdir(sesliharfler);
+
+            var alfabe = new List<char>();
+            for (int i = 97; i < 123; i++) alfabe.Add((char)i); //ascıı koda gore
+            //alfabe.ForEach(x => Console.WriteLine(x)); //lambda
+
+            koleksiyonyazdir(alfabe); //hata verir convert hatası interfacelerine bak getenuramator
+            //ortak kontratlara bak f12:IEnumrable list ve hashsetteki
+
+            //sadece turkce sesli harfleri iceren küme
+            //sesliharfler.ExceptWith(alfabe);
+            //Console.WriteLine("Alfabede yok");
+            //Console.WriteLine();
+            //koleksiyonyazdir(sesliharfler);
+
+            //birlesme
+            //sesliharfler.UnionWith(alfabe);
+          
+            //Console.WriteLine("BİRLESME");
+            //Console.WriteLine();
+            //koleksiyonyazdir(sesliharfler);
+
+            //KESİSİM
+            //sesliharfler.Intersect(alfabe);
+            
+            //Console.WriteLine("Alfabede yok");
+            //Console.WriteLine();
+            //koleksiyonyazdir(sesliharfler);
+
+            //simetrik
+            sesliharfler.SymmetricExceptWith(alfabe);
+            
+            Console.WriteLine("iki listede yok simetrik");
+            Console.WriteLine();
+            koleksiyonyazdir(sesliharfler);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+        static void koleksiyonyazdir(IEnumerable koleksiyon)
+        {
+            //enurable ortakdır
+            Console.WriteLine();
+            int i = 0;
+            foreach (char c in koleksiyon)
+            {
+                Console.Write($"{c,5}");
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Eleman SAyisi {i}");
+            //Console.WriteLine($"eleman sayim={koleksiyon.Count}");
+        }
+    }
+}
